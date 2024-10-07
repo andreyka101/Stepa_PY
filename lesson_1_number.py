7d3621b5835e2924eee48bcc0750a5b76a1a762c
andreyka101 <andrejkvasnin236@gmail.com>
2024-10-07 21:01:02 +0300

    first commit

---
+++ lesson_1_number.py
@@ -0,0 +1,96 @@
+# num_bool = True
+# num_int = 8
+# num_float = 7.3
+# num_str = "wer"
+
+
+
+# num_a1 = "5"
+# print(num_a1 * 3)
+# num_a1 = int(num_a1)
+# print(num_a1 * 3)
+
+
+
+# int_num = 9
+# print(int_num)
+# int_num = 7
+# intNum = 7
+
+
+
+
+
+
+# def fun_1(a = 5):
+#     print(a)
+# fun_1()
+
+
+
+# def fun_2(a , b ,s = 8):
+#     print(a)
+# fun_2(2,4)
+
+
+
+# def fun(num = 0):
+#     print(num)
+#     if(num < 10):
+#         return fun(num + 1)
+
+# fun()
+
+
+
+arr = [2,4,6,3,1,3,8]
+# arr.append(8)
+# # arr.append([4,6,4])
+# print(arr)
+
+
+# arr.extend([4,6,4])
+# print(arr)
+
+
+# print(arr.count(3))
+
+
+# print(arr.index(3 ,4))
+
+
+# arr.remove(3)
+# print(arr)
+
+
+# arr.pop()
+# print(arr)
+
+
+# arr.insert(1,999)
+# print(arr)
+
+
+# arr.sort()
+# print(arr)
+
+
+# arr.reverse()
+# print(arr)
+
+
+# arr.clear()
+# print(arr)
+
+
+# arr_2 = arr
+# arr_copy = arr.copy()
+# arr.clear()
+
+# print("arr = ",arr)
+# print("arr_2 = ",arr_2)
+# print("arr_copy = ",arr_copy)
+
+
+
+
