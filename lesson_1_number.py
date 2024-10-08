a4130faeb3148f9cbb35141ccd1d45c7c2596124
andreyka101 <andrejkvasnin236@gmail.com>
2024-10-08 11:41:55 +0300

    уаауау

--- lesson_1_number.py
+++ lesson_1_number.py
@@ -1,3 +1,4 @@
+# типы данных
 # num_bool = True
 # num_int = 8
 # num_float = 7.3
@@ -5,6 +6,7 @@
 
 
 
+# преобразование из одного типа в другой
 # num_a1 = "5"
 # print(num_a1 * 3)
 # num_a1 = int(num_a1)
@@ -12,6 +14,7 @@
 
 
 
+# название переменных
 # int_num = 9
 # print(int_num)
 # int_num = 7
@@ -21,68 +24,86 @@
 
 
 
+# функции
 
+# значение по умолчанию
 # def fun_1(a = 5):
 #     print(a)
 # fun_1()
 
 
 
+# передача нескольких значений функции
 # def fun_2(a , b ,s = 8):
 #     print(a)
 # fun_2(2,4)
 
 
 
+# рекурсивная функция
 # def fun(num = 0):
 #     print(num)
 #     if(num < 10):
 #         return fun(num + 1)
-
 # fun()
 
 
 
+
+
+# методы списка
+
 arr = [2,4,6,3,1,3,8]
+# arr.append(x) - Добавляет элемент x в конец списка
 # arr.append(8)
 # # arr.append([4,6,4])
 # print(arr)
 
 
+# arr.extend(x) - Расширяет список arr, добавляя в конец все элементы списка x
 # arr.extend([4,6,4])
 # print(arr)
 
 
+# arr.count(x) - Возвращает количество элементов со значением x
 # print(arr.count(3))
 
 
+# arr.index(x, i) - Возвращает положение первого элемента со значением x поиск начинается с индекса i (можно не писать)
 # print(arr.index(3 ,4))
 
 
+# arr.remove(x) - Удаляет первый элемент в списке, имеющий значение x. ValueError, если такого элемента не существует
 # arr.remove(3)
 # print(arr)
 
 
+# arr.pop(i) - 	Удаляет i-ый элемент и возвращает его. Если индекс не указан, удаляется последний элемент
 # arr.pop()
 # print(arr)
 
 
+# arr.insert(i, x) - Вставляет на i-ый элемент значение x
 # arr.insert(1,999)
 # print(arr)
 
 
+# arr.sort() - Сортирует список
 # arr.sort()
 # print(arr)
 
 
+# arr.reverse() - Разворачивает список
 # arr.reverse()
 # print(arr)
 
 
+# arr.clear() - Очищает список
 # arr.clear()
 # print(arr)
 
 
+# arr.copy() - Поверхностная копия списка
 # arr_2 = arr
 # arr_copy = arr.copy()
 # arr.clear()
@@ -93,4 +114,25 @@ arr = [2,4,6,3,1,3,8]
 
 
 
+
+
+# dz - функции
+# задачи на функции:
+# https://okpython.net/python/python_zadachnik/python_zadachnik.html#ex_12
+# https://pynative.com/python-functions-exercise-with-solutions/
+# https://proglib.io/p/funkcii-v-python-5-zadach-dlya-trenirovki-args-kwargs-i-lambda-funkciy-2022-06-15
+
+
+# задачи на рекурсию:
+# (внизу) https://proglib.io/p/samouchitel-po-python-dlya-nachinayushchih-chast-13-rekursivnye-funkcii-2023-01-23
+# https://w3resource.com/python-exercises/data-structures-and-algorithms/python-recursion.php
+
+
+
+
+
+# dz - списки
+# задачи на списки:
+# https://metanit.com/python/practice/4.php
+# https://pythonist.ru/python-spiski-zadachi-dlya-nachinayushhih/
 
