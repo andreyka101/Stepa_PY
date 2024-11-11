ae278e4166e4bbd86d35bda220cbf7e6c4857039
andreyka101 <andrejkvasnin236@gmail.com>
2024-11-11 21:50:22 +0300

    effff

--- lesson_7_data.py
+++ lesson_7_data.py
@@ -1,3 +1,5 @@
+
+# открытие файла
 # open_file = open("i_file.txt")
 # print(open_file)
 
@@ -15,23 +17,30 @@
 
 
 
+# по умолчанию файл открывается в режиме 'r'
 # open_file_2 = open("i_file.txt" , "r")
+# .read() - получаем текст отрытого файла
 # print(open_file_2.read())
 
 
-
+# переписываем файл
 # open_file_3 = open("i_file.txt" , "w")
 # print(open_file_3)
+# .write() = запись в файл
 # open_file_3.write("123456789")
 
+# .close() - закрывает файл
 # open_file_3.close()
 
+# если не закрыть файл то чтение не сработает
 # print("open =" , open("i_file.txt" , "r").read())
 
 
 
 
+# пример перезаписи и чтения
 # open_read = open("i_file.txt" , "r").read()
+# with - сам закрывает файл 
 # with open("i_file.txt" , "w") as file:
 #     file.write(open_read + "_5555www")
 
@@ -40,26 +49,50 @@
 
 
 
+# пример открытия на дозапись
 # with open("i_file.txt" , "a") as file:
 #     file.write("=poi")
 
 
 
+
+# \n - переносит строку
 # print("hello\n    world")
 # open_read_2 = [open("i_file.txt" , "r").read()]
 # print(open_read_2)
 
 
 
+# метод .readlines() каждую строку txt файла превращает в элемент массива
 # open_read_3 = open("i_file.txt" , "r").readlines()
 # print(open_read_3)
 
 
 
+# .split(s) - разделяет строку по строке s
 open_read_4 = open("i_file.txt" , "r").read()
 arr_open_read_4 = open_read_4.split("\n")
 print(arr_open_read_4)
 
 
 
+
+
+
+
+# дз
+# номер 1
+# сделать программу , которая принимает текст и сохраняет в файл 
+
+
+# номер 2
+# сделать программу калькулятор с историей программа должна показывать историю
+
+
+# номер 3
+# создайте менеджер пользователей
+# . программа должна работать до тех пор пока не не будет введено exit или ex
+# . если ввести new - создаётся новый пользователь он должен хранить имя и пароль (если ввести существующие имя то программа просит его изменить)
+# . если ввести del - удаляется пользователь его имя и пароль
+# все пользователи должны сохраняются на txt файл
 
