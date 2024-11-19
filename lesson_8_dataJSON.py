5142cdb5a1cdad7c27694eceaca8bdbc6c5ba00d
andreyka101 <andrejkvasnin236@gmail.com>
2024-11-19 13:13:47 +0300

    wdwwd

--- lesson_8_dataJSON.py
+++ lesson_8_dataJSON.py
@@ -3,11 +3,14 @@ import json
 # arr = [1,2,3,4]
 # print(type(arr))
 
+
+# json.dumps - переводит в json строку
 # str_json = json.dumps(arr)
 # print(type(str_json))
 
 
 
+# перезапись файла json
 # with open("j_file.json" , "w") as file:
 #     file.write(str_json)
 
@@ -19,17 +22,21 @@ import json
 #     "weqe": "222",
 #     45:"qwer"
 # }
+
+# пример со словарем
 # with open("j_file.json" , "w") as file:
 #     json.dump(obj , file)
 
 
 
+# json.loads - переводит json строку в list или dict
 # arr_2 = json.loads(str_json)
 # print(type(arr_2))
 
 
 
 
+# json.load - переводит открытый файл в list или dict
 # with open("j_file.json") as file:
 #     obj_2 = json.load(file)
 #     print(obj_2)
