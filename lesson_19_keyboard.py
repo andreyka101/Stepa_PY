612282b8e14bf836842ae5c43a09240bc0258bc1
andreyka101 <andrejkvasnin236@gmail.com>
2025-01-21 17:49:24 +0300

    efefeffe

--- lesson_19_keyboard.py
+++ lesson_19_keyboard.py
@@ -13,31 +13,47 @@ text.place(x=30,y=150)
 
 
 def fun_press(event):
+    # event - получаем информацию о обработчике (клавиша)
     # text.config(text=event)
+
+    # event.keysym - название клавиши
     text.config(text=event.keysym)
+
+    # event.keycode - номер клавиши
     # text.config(text=event.keycode)
+
+    # event.state - информация о дополнительно зажатых клавиш
     # text.config(text=event.state)
+
     if(event.keycode == 87):
         window.config(bg="#238ecc")
     # if(event.keysym == "w"):
     #     window.config(bg="#238ecc")
-
 
     # if(event.keycode == 87 and event.state == 9):
     #     window.config(bg="#6b1385")
     if(event.keysym == "W" and event.state == 9):
         window.config(bg="#6b1385")
 
+# обработчик нажатия клавиши клавиатуры 
 window.bind("<KeyPress>" , fun_press)
 
 
 
 
 def fun_release(event):
+    # event - получаем информацию о обработчике (клавиша)
     # text.config(text=event)
+
+    # event.keysym - название клавиши
     # text.config(text=event.keysym)
+
+    # event.keycode - номер клавиши
     # text.config(text=event.keycode)
+
+    # event.state - информация о дополнительно зажатых клавиш
     # text.config(text=event.state)
+
     # if(event.keycode == 87):
     #     window.config(bg="#238ecc")
     if(event.keysym == "w"):
@@ -49,6 +65,7 @@ def fun_release(event):
     # if(event.keysym == "W" and event.state == 9):
     #     window.config(bg="#6b1385")
 
+# обработчик отжатия кл авиши клавиатуры
 window.bind("<KeyRelease>" , fun_release)
 
 
