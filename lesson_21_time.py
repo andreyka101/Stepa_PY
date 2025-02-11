8d9efd193911d970d9307cb0a31ec9832aa6e202
andreyka101 <andrejkvasnin236@gmail.com>
2025-02-11 11:02:33 +0300

    цвцвцв

--- lesson_21_time.py
+++ lesson_21_time.py
@@ -3,13 +3,16 @@ import time
 # https://codelessons.dev/ru/rand-in-cplusplus/
 
 
+# .time() - получить unix время
 # print(time.time())
 start = 1
 end = 20
+# формула рандомного числа
 print(int(time.time() % (end - start + 1) + start))
 
 
 
+# .sleep(s) - программа останавливается на s секунд
 # print(time.time())
 # time.sleep(3)
 # print(time.time())
@@ -17,21 +20,24 @@ print(int(time.time() % (end - start + 1) + start))
 
 
 
-import datetime
 
+import datetime
 
 # data_time_now = datetime.datetime.now()
 
 
+# получаем дату и время
 # print(data_time_now)
 
 
 
+# получаем дату
 # print(data_time_now.date())
 # print(data_time_now.date().day)
 
 
 
+# получаем время
 # print(data_time_now.time())
 # print(data_time_now.time().minute)
 # print(data_time_now.time().second)
